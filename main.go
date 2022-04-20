7a6f3ce1a67898aa35cd960d474ca32688d1b4d1
myoan <motoki.yoan@gmail.com>
2022-04-21 08:03:47 +0900

    Add rules

--- main.go
+++ main.go
@@ -1,29 +1,60 @@
 package main
 
 import (
+	"fmt"
+	"math/rand"
 	"net/http"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 var (
-	counter = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "myCounter",
+	counter = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "my_counter",
 		Help: "counter sample",
 	})
+	diceGaugeVec = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "dice",
+		Help: "gauge sample",
+	}, []string{"env", "side"})
 )
+
+func DiceGauge(env string, side int, ret float64) {
+	diceGaugeVec.WithLabelValues(env, fmt.Sprintf("%d", side)).Set(ret)
+}
+
+func init() {
+	prometheus.MustRegister(counter)
+	prometheus.MustRegister(diceGaugeVec)
+}
+
+func diceRoll() {
+	environments := []string{"dev", "stg", "prod"}
+	sides := []int{6, 8, 12, 24}
+
+	for _, env := range environments {
+		for _, side := range sides {
+			ret := rand.Intn(side)
+			DiceGauge(env, side, float64(ret))
+		}
+	}
+}
 
 func main() {
 	go func() {
 		for {
 			counter.Inc()
+			diceRoll()
 			time.Sleep(1 * time.Second)
 		}
 	}()
 
+	fmt.Println("Start APP server")
+
 	http.Handle("/metrics", promhttp.Handler())
-	http.ListenAndServe(":8000", nil)
+	if err := http.ListenAndServe(":8000", nil); err != nil {
+		fmt.Println("Error: ", err)
+	}
 }
